b1e7cbaf2ff40ba7875c7e54c364e89c9014a84b
Dima Litvinov <dlv@vava.cars>
2022-12-04 22:37:09 +0200

    feat: add read method

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
-const read = async () => {
-    // Write your code here 
+import path from 'path';
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+
+import { FILES } from '../constants/path-files.constants.js';
+import { getPath } from '../utils/get-path.util.js';
+import { CustomValidationError } from '../errors/custom-validation-error.js';
+
+export const read = async () => {
+  const sourcePath = path.resolve(getPath(import.meta.url), FILES, 'fileToRead.txt');
+
+  try {
+    const isFilesExists = existsSync(sourcePath);
+
+    if (!isFilesExists) {
+      throw new CustomValidationError('FS operation failed');
+    }
+
+    const file = await readFile(sourcePath);
+
+    process.stdout.write(file);
+  } catch (err) {
+    throw err;
+  }
 };
 
-await read();
+read();
