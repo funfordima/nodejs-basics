a648928568cddc6223d65ce0db16b86dc61f5ad7
Dima Litvinov <dlv@vava.cars>
2022-12-04 22:51:24 +0200

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
+import path from 'path';
+import fs from 'fs';
+import { createHash } from 'crypto';
+
+import { FILES } from '../constants/path-files.constants.js';
+import { getPath } from '../utils/get-path.util.js';
+
+export const calculateHash = async () => {
+  const sourcePath = path.resolve(getPath(import.meta.url), FILES, 'fileToCalculateHashFor.txt');
+  const fileBuffer = fs.readFileSync(sourcePath);
+  const hash = createHash('sha256');
+
+  hash.update(fileBuffer);
+
+  const hex = `${hash.digest('hex')}`;
+  process.stdout.write(hex);
+  return hex;
 };
 
-await calculateHash();
+calculateHash();
