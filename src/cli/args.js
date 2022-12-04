ad8f9cdf6f4e908b729235c17bd24a4ad9ec52b0
Dima Litvinov <dlv@vava.cars>
2022-12-04 22:44:09 +0200

    feat: add cli module

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
-const parseArgs = () => {
-    // Write your code here 
+export const parseArgs = () => {
+  const entryString = '--';
+  const myArgs = process.argv.slice(2);
+  const mappedArgs = myArgs.reduce((acc, val, i, arr) => {
+    if (val.startsWith(entryString)) {
+      const isArgumentsValid = (i < arr.length - 1) && !arr[i + 1].startsWith(entryString);
+      return isArgumentsValid ?
+        (acc.push(`${val} is ${arr[i + 1]}`), acc) :
+        (acc.push(`${val} is undefined`), acc);
+    }
+
+    return acc;
+  }, []);
+
+  process.stdout.write(mappedArgs.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
-const parseEnv = () => {
-    // Write your code here 
+export const parseEnv = () => {
+  const envVariables = Object.entries(process.env);
+  const filterByParam = (data) => data.startsWith('RSS_');
+  const filteredList = envVariables.filter((entry) => entry.find(filterByParam));
+  const mappedValues = filteredList.map((entry) => entry.join('='));
+
+  process.stdout.write(mappedValues.join(' '));
 };
 
 parseEnv();
