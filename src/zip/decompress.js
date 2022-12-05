ad21dce3dc072e338afee3aadc81642608b6aa3b
Dima Litvinov <dlv@vava.cars>
2022-12-05 22:05:02 +0200

    feat: add zib functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { pipeline } from 'stream';
+import zlib from 'zlib';
+
+import { FILES } from '../constants/path-files.constants.js';
+import { getPath } from '../utils/get-path.util.js';
+
+export const decompress = async () => {
+  const sourcePath = path.resolve(getPath(import.meta.url), FILES, 'archive.gz');
+  const targetPath = path.resolve(getPath(import.meta.url), FILES, 'fileToCompress.txt');
+  const readStream = createReadStream(sourcePath);
+  const writeStream = createWriteStream(targetPath);
+
+  pipeline(
+    readStream,
+    zlib.createGunzip(),
+    writeStream,
+    (err) => {
+      if (err) {
+        process.stderr.write(err);
+      }
+    });
 };
 
-await decompress();
+decompress();
